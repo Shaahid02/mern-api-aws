256a27cafe10ee869f74536f9666b0661ed9583f
Shaahid02 <122220522+Shaahid02@users.noreply.github.com>
2023-11-03 10:51:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yn4n60l.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://Digby:dckryIEOHrEjSOne@cluster0.yn4n60l.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(process.env.PORT || 5000);
