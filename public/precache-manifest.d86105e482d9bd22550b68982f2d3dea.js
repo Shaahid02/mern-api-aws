3ed74eb4cea093be5393f7850d19cbf2c8f20b1e
shaahid <shaahidfathul@gmail.com>
2023-11-09 10:35:31 +0530

    Added frontend

---
+++ public/precache-manifest.d86105e482d9bd22550b68982f2d3dea.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abcaf13fc048884a1e24447d1ca6422f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e9649914503c1dff336",
+    "url": "/static/css/3.38453735.chunk.css"
+  },
+  {
+    "revision": "002353f9f7bbead7e8d6",
+    "url": "/static/css/4.090fe32b.chunk.css"
+  },
+  {
+    "revision": "410fb7a256c42e33403f",
+    "url": "/static/css/5.41820a8c.chunk.css"
+  },
+  {
+    "revision": "4daeba56f21fed1c0579",
+    "url": "/static/css/6.9378fba0.chunk.css"
+  },
+  {
+    "revision": "2a22364507e2fd7b091e",
+    "url": "/static/css/7.295134cd.chunk.css"
+  },
+  {
+    "revision": "a17644b2e42992e6e4ac",
+    "url": "/static/css/main.939e3537.chunk.css"
+  },
+  {
+    "revision": "34fea44d72ad2b7f656e",
+    "url": "/static/js/2.1ed87e2e.chunk.js"
+  },
+  {
+    "revision": "9e9649914503c1dff336",
+    "url": "/static/js/3.25970cc9.chunk.js"
+  },
+  {
+    "revision": "002353f9f7bbead7e8d6",
+    "url": "/static/js/4.51befa63.chunk.js"
+  },
+  {
+    "revision": "410fb7a256c42e33403f",
+    "url": "/static/js/5.e84a13f6.chunk.js"
+  },
+  {
+    "revision": "4daeba56f21fed1c0579",
+    "url": "/static/js/6.3eb123c8.chunk.js"
+  },
+  {
+    "revision": "2a22364507e2fd7b091e",
+    "url": "/static/js/7.984faea6.chunk.js"
+  },
+  {
+    "revision": "a17644b2e42992e6e4ac",
+    "url": "/static/js/main.41758463.chunk.js"
+  },
+  {
+    "revision": "2a4353b2bc364b772926",
+    "url": "/static/js/runtime-main.880a064d.js"
+  }
+]);
